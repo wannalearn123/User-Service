956341687590be6458ba4728991f02c48399b6c4
unknown <nurdiansyahagung277@gmail.com>
2026-01-04 08:13:06 +0700

    Update: table peran menu

--- src/app/Database/Migrations/2025-12-29-224634_AkunPeran.php
+++ src/app/Database/Migrations/2025-12-29-224634_AkunPeran.php
@@ -8,11 +8,49 @@ class AkunPeran extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'    => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'akun_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'peran_id'   => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'identitas' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('akun_id', 'akun', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('peran_id', 'peran', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('akun_peran');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('akun_peran');
     }
 }
--- src/app/Database/Migrations/2025-12-29-224648_PeranMenu.php
+++ src/app/Database/Migrations/2025-12-29-224648_PeranMenu.php
@@ -8,11 +8,64 @@ class PeranMenu extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'    => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'peran_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'menu_id'   => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'c' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+                'default'    => 0,
+            ],
+            'r' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+                'default'    => 0,
+            ],
+            'u' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+                'default'    => 0,
+            ],
+            'd' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+                'default'    => 0,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('peran_id', 'peran', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('menu_id', 'menu', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('peran_menu');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('peran_menu');
     }
 }
