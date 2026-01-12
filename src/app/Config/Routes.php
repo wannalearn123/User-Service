64e6ae275bb88d75d148bd158d7140644b0715f3
fadhil <163954514+steve01-debug@users.noreply.github.com>
2026-01-12 20:41:10 +0700

    Routes.php

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -5,4 +5,18 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+// --- Rute User & Authentication Service ---
+
+$routes->group('api', function($routes) {
+    
+    // Endpoint Publik (Login & Register) [cite: 58, 59]
+    $routes->post('auth/login', 'UserService\AuthController::login');
+    $routes->post('auth/register', 'UserService\AuthController::register');
+
+    // Endpoint Terproteksi (Butuh AuthFilter) 
+    // Pastikan alias 'auth' sudah didaftarkan di Config/Filters.php
+    $routes->group('users', ['filter' => 'auth'], function($routes) {
+        $routes->get('(:any)', 'UserService\UserController::show/$1'); // GET /api/users/{id}
+    });
+});
